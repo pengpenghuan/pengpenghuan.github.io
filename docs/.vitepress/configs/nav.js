6fac86b574d1cf2be1e7c12f7ed6f94b52e2c688
penghuan <huan.peng@cy-tech.net>
2023-09-07 13:51:05 +0800

    自动构建和发布配置文件

--- docs/.vitepress/configs/nav.js
+++ docs/.vitepress/configs/nav.js
@@ -3,6 +3,7 @@
  * attention: id字段必须为数值，或者数值字符串
  * */
 export default [
+  { id: 9999, text: '首页', link: '/' },
   { id: 1, text: '视觉AI平台', link: '/video/', activeMatch: '^/video/' },
   { id: 2, text: '生产应急平台', link: '/emergency/', activeMatch: '^/emergency/' },
   // { id: 3, text: '架构设计', link: '/emergency/', activeMatch: '^/emergency/' },
